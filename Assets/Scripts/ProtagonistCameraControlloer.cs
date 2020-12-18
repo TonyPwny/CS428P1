4711a06876b7961457e13344b7cb560df6756680
Anthony Tiongson <anthonytiongson@me.com>
2020-12-18 02:23:33 -0500

    base protagonist control via user

---
+++ Assets/Scripts/ProtagonistCameraControlloer.cs
@@ -0,0 +1,38 @@
+﻿// Anthony Tiongson (ast119)
+// List of resources used:
+//      
+//      
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtagonistCameraControlloer : MonoBehaviour
+{
+    public GameObject protagonist;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - protagonist.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // LateUpdate is called once per frame but is guaranteed to run after all items have been processed in Update
+    void LateUpdate()
+    {
+        transform.position = protagonist.transform.position + offset;
+
+        if (protagonist.transform.position.y < -5)
+        {
+            transform.position = new Vector3(0, 8, -12);
+        }
+    }
+}
---
+++ Assets/Scripts/ProtagonistController.cs
@@ -0,0 +1,62 @@
+﻿// Anthony Tiongson (ast119)
+// List of resources used:
+//      
+//      
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtagonistController : MonoBehaviour
+{
+    public float speed;
+    public float jumpPower;
+
+    private Rigidbody rb;
+    private bool isGrounded = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // FixedUpdate is called just before performing any physics calculations
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("1_Horizontal");
+        float moveVertical = Input.GetAxis("1_Vertical");
+
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        if (Input.GetButton("1_Jump") && isGrounded)
+        {
+            movement.Set(moveHorizontal, jumpPower, moveVertical);
+        }
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isGrounded = true;
+        }
+    }
+}
